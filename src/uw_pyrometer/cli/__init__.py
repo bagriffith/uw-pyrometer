80896e2f634dec521fbabdb34d016874b37518ea
Brady Griffith <bradyagriffith@gmail.com>
2024-02-14 09:31:10 -0800

    Added options for sample interval and number.

--- src/uw_pyrometer/cli/__init__.py
+++ src/uw_pyrometer/cli/__init__.py
@@ -17,22 +17,28 @@ def uw_pyrometer():
 @click.option('--device_id', '-d', default=0, type=click.IntRange(0, 255))
 @click.option('--verbose', '-v', default=False, is_flag=True)
 @click.option('--broadcast', '-b', default=False, is_flag=True)
-def measure(serial_path, device_id, verbose, broadcast):
+@click.option('--interval', '-i', default=1.0, type=click.FloatRange(min_open=0),
+              help='Polling interval in seconds.')
+@click.option('--samples', '-n', default=1, type=click.IntRange(min=0),
+              help='Number of samples to collect. 0 runs indefinitely.')
+def measure(serial_path, device_id, verbose, broadcast, interval, samples):
     if verbose:
         pyrometer.logger.setLevel('DEBUG')
         pyrometer.logger.addHandler(logging.StreamHandler())
 
     device = pyrometer.PyrometerSerial(device_id, serial_path)
-    while click:
+    samples_taken = 0
+    while (samples_taken < samples) or (samples == 0):
+        if samples_taken > 0:
+            time.sleep(interval)
+            click.echo()
         try:
             ref, tr, tp = device.get_measurement(convert=True, broadcast=broadcast)
             click.echo(f'Reference:   {ref:1.3f}')
             click.echo(f'Thermistor:  {tr:1.3f}')
             click.echo(f'Thermopile:  {tp:1.3f}')
-            click.echo()
+            samples_taken += 1
         except TimeoutError:
             click.echo('Read timed out.')
-        time.sleep(1.)
-
 
 uw_pyrometer.add_command(measure)
